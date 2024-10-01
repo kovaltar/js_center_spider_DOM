23f68c6a105a6c9f145fb25abfbdceaca2215a06
kovaltar <kovaltar@gmail.com>
2024-10-02 01:15:17 +0300

    make build

---
+++ index.f8f25609.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),l=(e.clientWidth-t.clientWidth)/2,i=(e.clientHeight-t.clientHeight)/2;t.style.left=l+"px",t.style.top=i+"px";
+//# sourceMappingURL=index.f8f25609.js.map
